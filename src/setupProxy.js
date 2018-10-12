2eab399999b478dacaf7542033dfc94c1d1502d0
Brian Wald <bwald@gitlab.com>
2018-10-12 12:35:30 -0400

    remove port

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api', { target: 'http://microservices-ui.35.239.64.228.nip.io:5000/' }));
+  app.use(proxy('/api', { target: 'http://microservices-ui.35.239.64.228.nip.io/' }));
 };
