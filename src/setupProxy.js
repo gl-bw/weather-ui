b7e337296e6f8e8d337c6481b5cb4c8483218461
Brian Wald <bwald@gitlab.com>
2018-10-12 12:42:03 -0400

    fix url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api', { target: 'http://microservices-ui.35.239.64.228.nip.io/' }));
+  app.use(proxy('/api', { target: 'http://microservices-api.35.239.64.228.nip.io/' }));
 };
